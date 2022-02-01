d31d3317efcbb1494cc570b9856e2880eddf8b6c
Arturo Mata <81439664+matarturo@users.noreply.github.com>
2022-02-01 07:43:12 -0400

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+Script: ping.sh
+Autor: Arturo Mata <arturo.mata@gmail.com>
+Version: 1.0.0
+Repositorio: https://github.com/matarturo/
+Released under the GNU General Public License WITHOUT ANY WARRANTY.
+See LICENSE.TXT for details.
+
+# #Script escrito en lenguaje Bash para detectar hosts activos usando el comando <ping>
+
 for ip in `seq 1 254`
 do
     ping -c 1 192.168.43.$ip > /dev/null
